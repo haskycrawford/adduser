978b9a2a547e3042416468cd0a301fc534c31ba1
KF <KLEITON.FREITAS@LIVE.COM>
2017-03-27 10:05:25 -0300

    Add files via upload
    versão atualizada com senha de 6 dígitos alfanuméricas aleatórias.
    validação se o arquivo a ser executado é valido.

---
+++ adduser v2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+#
+# @date: March 25, 2017
+# @version:1.01
+# @license: GPL v3 <http://www.gnu.org/licenses>
+#
+# @autor: Kleiton Freitas
+# @e-mail: kleiton.freitas@live.com
+# @faculdade dos guararapes / 4NA
+# @prof. Bosco, João
+#
+#
+#
+
+XCAT=$(cat $1 2> /dev/null)
+if [ -e $1 ]
+then
+	echo " Iniciando cadastro de usuarios..."
+
+	for USUARIO in $XCAT
+		do
+		SENHA=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c6)
+		useradd $USUARIO
+		echo "$USUARIO:$SENHA" | chpasswd
+		echo " Nome usuario: $USUARIO, Senha: $SENHA"
+		sleep 1
+		sed -i 's/'$USUARIO'/'$USUARIO'\t'$SENHA'/g' $1 
+		echo " Usuario cadastrado com sucesso!"
+		
+		if [ $? -ne 0 ] 
+		then 
+			echo " Erro!"
+			exit
+		fi	
+
+	done
+
+else
+	echo " !!! Arquiro não encontrado ou inexistente !!!"
+	exit
+fi
